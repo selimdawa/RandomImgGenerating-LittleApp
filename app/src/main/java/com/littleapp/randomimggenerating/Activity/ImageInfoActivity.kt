cc2e05ea5895f761cd6b13f9652fef228c7df8bc
selimdawa <selimdawa@gmail.com>
2026-08-29 06:58:55 +0300

    Edge to Edge - Add

--- app/src/main/java/com/littleapp/randomimggenerating/Activity/ImageInfoActivity.kt
+++ app/src/main/java/com/littleapp/randomimggenerating/Activity/ImageInfoActivity.kt
@@ -3,7 +3,10 @@ package com.littleapp.randomimggenerating.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.littleapp.randomimggenerating.R
 import com.littleapp.randomimggenerating.Unit.DATA
 import com.littleapp.randomimggenerating.Unit.THEME
@@ -17,10 +20,17 @@ class ImageInfoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         THEME.setThemeOfApp(this)
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
 
         _binding = ActivityImageInfoBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         binding.toolbar.nameSpace.text = getString(R.string.image_info)
 
--- app/src/main/java/com/littleapp/randomimggenerating/Activity/SplashActivity.kt
+++ app/src/main/java/com/littleapp/randomimggenerating/Activity/SplashActivity.kt
@@ -3,7 +3,10 @@ package com.littleapp.randomimggenerating.Activity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.littleapp.randomimggenerating.Unit.CLASS
 import com.littleapp.randomimggenerating.Unit.THEME
 import com.littleapp.randomimggenerating.Unit.VOID
@@ -19,9 +22,16 @@ class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         THEME.setThemeOfApp(this)
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         _binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         Handler(Looper.getMainLooper()).postDelayed({ launch() }, timeFinal.toLong())
     }
